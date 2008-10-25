ae8747146b3ff6f4a1ca4b6323b1db07c480c325
ivan.teoh <ivan.teoh@54d4be74-a0c4-11dd-988f-d3ef9af6d102>
2008-10-25 09:29:39 +0000

    - add order detail. - update order detail.

---
+++ trunk/src/MillionBeauty/InsertOrderDetailForm.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using System.Globalization;
+
+namespace MillionBeauty
+{
+    class InsertOrderDetailForm : OrderDetailForm
+    {
+        public event EventHandler Added;
+        
+        public InsertOrderDetailForm()
+            : base()
+        {
+            Load += InsertOrderDetailFormLoad; 
+        }
+
+        private void InsertOrderDetailFormLoad(object sender, EventArgs e)
+        {
+            Quantity = "0";
+            DiscountPercent = "0";
+            TotalCost = "0";
+        }
+
+        protected override void AddButtonClicked()
+        {
+            base.AddButtonClicked();
+
+            if (string.IsNullOrEmpty(ProductId))
+            {
+                MessageBox.Show(
+                    "Pick a product before adding to order form.",
+                    Properties.Resources.Title,
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error,
+                    MessageBoxDefaultButton.Button1,
+                    MessageBoxOptions.RightAlign & MessageBoxOptions.RtlReading);
+            }
+            else if (Convert.ToInt64(Quantity, CultureInfo.InvariantCulture) == 0)
+            {
+                MessageBox.Show(
+                    "Quantity can not be zero.",
+                    Properties.Resources.Title,
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error,
+                    MessageBoxDefaultButton.Button1,
+                    MessageBoxOptions.RightAlign & MessageBoxOptions.RtlReading);                
+            }
+            else
+            {
+                EventHandler eventHandler = Added;
+                if (eventHandler != null)
+                {
+                    eventHandler(this, EventArgs.Empty);
+                }
+
+                Close();
+            }
+        }
+
+        
+    }
+}
--- trunk/src/MillionBeauty/OrderForm.cs
+++ trunk/src/MillionBeauty/OrderForm.cs
@@ -19,12 +19,12 @@ namespace MillionBeauty
             orderDetailsControl.DataSetSource = orderDetails;
             customerFindButton.Click += CustomerPickButtonClick;
             orderDetailsControl.AddButtonClicked += OrderDetailsControlAddButtonClicked;
+            orderDetailsControl.EnterKeyDowned += OrderDetailsControlEnterKeyDowned;
             KeyDown += OrderFormKeyDown;
-        }
+        }        
 
         private BindingList<OrderDetail> orderDetails;
-        private string customerId;
-        private BindingSource bindingSource;
+        private string customerId;        
             
         private void CustomerPickButtonClick(object sender, EventArgs e)
         {
@@ -49,29 +49,76 @@ namespace MillionBeauty
 
         private void OrderDetailsControlAddButtonClicked(object sender, EventArgs e)
         {
-            OrderDetailForm orderDetailForm = new OrderDetailForm();
-            orderDetailForm.Added += OrderDetailFormAdded;
-            orderDetailForm.ShowDialog(this);
-            orderDetailForm.Added -= OrderDetailFormAdded;
-            
+            InsertOrderDetailForm insertOrderDetailForm = new InsertOrderDetailForm();
+            insertOrderDetailForm.Added += InsertOrderDetailFormAdded;
+            insertOrderDetailForm.ShowDialog(this);
+            insertOrderDetailForm.Added -= InsertOrderDetailFormAdded;
         }
 
-        private void OrderDetailFormAdded(object sender, EventArgs e)
+        private void InsertOrderDetailFormAdded(object sender, EventArgs e)
         {
-            OrderDetailForm orderDetailForm = sender as OrderDetailForm;
+            InsertOrderDetailForm insertOrderDetailForm = sender as InsertOrderDetailForm;
             OrderDetail orderDetail = new OrderDetail();
-            orderDetail.ProductId = Convert.ToInt64(orderDetailForm.ProductId, CultureInfo.InvariantCulture);
-            orderDetail.Product = orderDetailForm.Product;
-            orderDetail.Description = orderDetailForm.Description;
-            orderDetail.ProductType = orderDetailForm.ProductType;
-            orderDetail.InStock = Convert.ToInt64(orderDetailForm.InStock, CultureInfo.InvariantCulture);
-            orderDetail.Price = Convert.ToDecimal(orderDetailForm.Price, CultureInfo.InvariantCulture);
-            orderDetail.Quantity = Convert.ToInt64(orderDetailForm.Quantity, CultureInfo.InvariantCulture);
-            orderDetail.DiscountPercent = Convert.ToDecimal(orderDetailForm.DiscountPercent, CultureInfo.InvariantCulture);
-            orderDetail.TotalCost = Convert.ToDecimal(orderDetailForm.TotalCost, CultureInfo.InvariantCulture);
+            orderDetail.ProductId = Convert.ToInt64(insertOrderDetailForm.ProductId, CultureInfo.InvariantCulture);
+            orderDetail.Product = insertOrderDetailForm.Product;
+            orderDetail.Description = insertOrderDetailForm.Description;
+            orderDetail.ProductType = insertOrderDetailForm.ProductType;
+            orderDetail.InStock = Convert.ToInt64(insertOrderDetailForm.InStock, CultureInfo.InvariantCulture);
+            orderDetail.Price = Convert.ToDecimal(insertOrderDetailForm.Price, CultureInfo.InvariantCulture);
+            orderDetail.Quantity = Convert.ToInt64(insertOrderDetailForm.Quantity, CultureInfo.InvariantCulture);
+            orderDetail.DiscountPercent = Convert.ToDecimal(insertOrderDetailForm.DiscountPercent, CultureInfo.InvariantCulture);
+            orderDetail.TotalCost = Convert.ToDecimal(insertOrderDetailForm.TotalCost, CultureInfo.InvariantCulture);
 
             orderDetails.Add(orderDetail);
-        }       
+        }
+
+        private void OrderDetailsControlEnterKeyDowned(object sender, EventArgs e)
+        {
+            UpdateOrderDetailForm updateOrderDetailForm = new UpdateOrderDetailForm();
+            updateOrderDetailForm.Inserted += UpdateOrderDetailFormInserted;
+            updateOrderDetailForm.Updated += UpdateOrderDetailFormUpdated;
+            updateOrderDetailForm.ShowDialog(this);
+            updateOrderDetailForm.Inserted -= UpdateOrderDetailFormInserted;
+            updateOrderDetailForm.Updated -= UpdateOrderDetailFormUpdated;
+        }
+
+        private void UpdateOrderDetailFormInserted(object sender, EventArgs e)
+        {
+            DataGridViewRow selectedRow = orderDetailsControl.SelectedRow;
+            if (selectedRow != null)
+            {
+                UpdateOrderDetailForm updateOrderDetailForm = sender as UpdateOrderDetailForm;
+                updateOrderDetailForm.ProductId = selectedRow.Cells[0].Value.ToString();
+                updateOrderDetailForm.Product = selectedRow.Cells[1].Value.ToString();
+                updateOrderDetailForm.Description = selectedRow.Cells[2].Value.ToString();
+                updateOrderDetailForm.ProductType = selectedRow.Cells[3].Value.ToString();
+                updateOrderDetailForm.InStock = selectedRow.Cells[4].Value.ToString();
+                updateOrderDetailForm.Price = selectedRow.Cells[5].Value.ToString();
+                updateOrderDetailForm.Quantity = selectedRow.Cells[6].Value.ToString();
+                updateOrderDetailForm.DiscountPercent = selectedRow.Cells[7].Value.ToString();
+                updateOrderDetailForm.TotalCost = selectedRow.Cells[8].Value.ToString();
+                Int64 left = Convert.ToInt64(selectedRow.Cells[4].Value, CultureInfo.InvariantCulture) + Convert.ToInt64(selectedRow.Cells[6].Value, CultureInfo.InvariantCulture);
+                updateOrderDetailForm.DefaultInStock = left;
+            }            
+        }
+
+        private void UpdateOrderDetailFormUpdated(object sender, EventArgs e)
+        {
+            DataGridViewRow selectedRow = orderDetailsControl.SelectedRow;
+            if (selectedRow != null)
+            {
+                UpdateOrderDetailForm updateOrderDetailForm = sender as UpdateOrderDetailForm;
+                selectedRow.Cells[0].Value = Convert.ToInt64(updateOrderDetailForm.ProductId, CultureInfo.InvariantCulture);
+                selectedRow.Cells[1].Value = updateOrderDetailForm.Product;
+                selectedRow.Cells[2].Value = updateOrderDetailForm.Description;
+                selectedRow.Cells[3].Value = updateOrderDetailForm.ProductType;
+                selectedRow.Cells[4].Value = Convert.ToInt64(updateOrderDetailForm.InStock, CultureInfo.InvariantCulture);
+                selectedRow.Cells[5].Value = Convert.ToDecimal(updateOrderDetailForm.Price, CultureInfo.InvariantCulture);
+                selectedRow.Cells[6].Value = Convert.ToInt64(updateOrderDetailForm.Quantity, CultureInfo.InvariantCulture);
+                selectedRow.Cells[7].Value = Convert.ToDecimal(updateOrderDetailForm.DiscountPercent, CultureInfo.InvariantCulture);
+                selectedRow.Cells[8].Value = Convert.ToDecimal(updateOrderDetailForm.TotalCost, CultureInfo.InvariantCulture);
+            }      
+        }
 
         private void OrderFormKeyDown(object sender, KeyEventArgs e)
         {
