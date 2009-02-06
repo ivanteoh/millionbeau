6b086b05d4ea80151fe0bf3c62f7681de860281c
ivan.teoh <ivan.teoh@54d4be74-a0c4-11dd-988f-d3ef9af6d102>
2009-02-06 07:25:14 +0000

    - MB 2.0 will be in WPF. - add new WPF GUI.

---
+++ trunk/src/MainGui/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿// <summary>
+//     Main window.
+// </summary>
+//-----------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MillionBeauty
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow 
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+#if (!DEBUG)          
+            if (Properties.Settings.Default.GotDatabase)
+            {
+                this.Source = new Uri("Pages/MainPage.xaml", UriKind.Relative);                
+            }
+            else
+            {
+                Properties.Settings.Default.Upgrade();
+                this.Source = new Uri("Pages/DatabasePage.xaml", UriKind.Relative);                
+            }
+#else
+            // Debug code: Straight away run main form
+            Properties.Settings.Default.Database = @"D:\work\Data\MillionBeautyDB.db";
+            //this.Source = new Uri("Pages/MainPage.xaml", UriKind.Relative);
+            this.Source = new Uri("Pages/DatabasePage.xaml", UriKind.Relative);
+#endif            
+        }        
+    }
+}
