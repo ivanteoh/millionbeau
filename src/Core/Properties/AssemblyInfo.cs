1434ebd0015200c3addb6d996bd4a7791199d9a8
ivan.teoh <ivan.teoh@54d4be74-a0c4-11dd-988f-d3ef9af6d102>
2009-02-06 09:37:30 +0000

    - Add verify database method when load database is clicked.

--- src/Core/Properties/AssemblyInfo.cs
+++ src/Core/Properties/AssemblyInfo.cs
@@ -2,6 +2,7 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+//using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -37,3 +38,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyFileVersion("1.1.0.0")]
 
 [assembly: CLSCompliant(true)]
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
